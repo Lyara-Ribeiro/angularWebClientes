f34f01817dfb28f2c12468217016dd16931fb2ce
Lyara-Ribeiro <lyaragithub@gmail.com>
2024-10-31 12:50:26 -0300

    Criacao e integracao de cadastro e autenticacao de usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { CadastrarClientesComponent } from './cadastrar-clientes/cadastrar-clientes.component';
 import { ConsultarClientesComponent } from './consultar-clientes/consultar-clientes.component';
 import { EditarClientesComponent } from './editar-clientes/editar-clientes.component';
+import { AutenticarUsuarioComponent } from './autenticar-usuario/autenticar-usuario.component';
+import { CriarUsuarioComponent } from './criar-usuario/criar-usuario.component';
+
 
 export const routes: Routes = [
-{
-    path: 'app/cadastrar-clientes',
-    component: CadastrarClientesComponent
-},
-{
-    path: 'app/consultar-clientes',
-    component: ConsultarClientesComponent
-},
-{
-    path: 'app/editar-clientes/:id',
-    component: EditarClientesComponent
-},
-{
-    path: '', pathMatch:'full',
-    redirectTo: '/app/consultar-clientes'
-}
+    {
+        path: 'app/autenticar-usuario',
+        component: AutenticarUsuarioComponent
+    },
+    {
+        path: 'app/criar-usuario',
+        component: CriarUsuarioComponent
+    },
+    {
+        path: 'app/cadastrar-clientes',
+        component: CadastrarClientesComponent
+    },
+    {
+        path: 'app/consultar-clientes',
+        component: ConsultarClientesComponent
+    },
+    {
+        path: 'app/editar-clientes/:id',
+        component: EditarClientesComponent
+    },
+    {
+        path: '', pathMatch: 'full', //página inicial do projeto
+        redirectTo: '/app/autenticar-usuario'
+    }
 ];
+
+
+
+
+
